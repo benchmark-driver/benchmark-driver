d1e60433b04e87209983057e5ed9fe52f04c1ca7
Takashi Kokubun <takashikkbn@gmail.com>
2017-12-16 23:30:29 +0900

    Add -o, --output option

--- lib/benchmark/runner.rb
+++ lib/benchmark/runner.rb
@@ -2,6 +2,8 @@ module Benchmark::Runner
   # Benchmark::Runner is pluggable.
   # Create `Benchmark::Runner::FooBar` as benchmark-runner-foo_bar.gem and specify `runner: foo_bar`.
   #
+  # Currently this has no options other than Exec, but this will have Eval for sure.
+  #
   # @param [Symbol] name
   def self.find(name)
     class_name = Benchmark::Driver::Configuration.camelize(name.to_s)
