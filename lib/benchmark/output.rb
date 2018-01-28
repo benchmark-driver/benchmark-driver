bc8f66e87591aaf9a4f873abaf13dd8b694eca0e
Takashi Kokubun <takashikkbn@gmail.com>
2018-01-28 23:11:35 +0900

    Automatically require output plugin

--- lib/benchmark/output.rb
+++ lib/benchmark/output.rb
@@ -6,6 +6,9 @@ module Benchmark::Output
     # @param [Symbol] name
     def find(name)
       class_name = Benchmark::Driver::Configuration.camelize(name.to_s)
+      unless Benchmark::Output.const_defined?("#{class_name}::REQUIRED_FIELDS")
+        require "benchmark/output/#{name}"
+      end
       Benchmark::Output.const_get(class_name, false)
     end
   end
