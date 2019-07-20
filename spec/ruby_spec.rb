4e69b8e32395f91e75dfe5c91385fdff71c1197b
Takashi Kokubun <takashikkbn@gmail.com>
2019-07-20 15:15:40 +0900

    Polyfill with_clean_env for spec

--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -3,6 +3,18 @@ describe 'Ruby interface' do
     assert_execute(RbConfig.ruby, *args)
   end
 
+  def with_clean_env(&block)
+    if Bundler.respond_to?(:with_unbundled_env)
+      Bundler.with_unbundled_env do
+        block.call
+      end
+    else
+      Bundler.with_clean_env do
+        block.call
+      end
+    end
+  end
+
   Dir.glob(File.expand_path('./fixtures/ruby/*.rb', __dir__)).each do |script|
     it "runs #{File.basename(script)} with default options" do
       ruby script
@@ -12,7 +24,7 @@ describe 'Ruby interface' do
   # for `make run` with test.rb requiring ruby/ruby/benchmark/lib/load.rb
   it 'supports --disable-gems' do
     lib_dir = File.expand_path('../lib', __dir__)
-    Bundler.with_unbundled_env do
+    with_clean_env do
       Tempfile.open(['benchmark_driver-test', '.rb']) do |f|
         f.puts <<~EOS
           $:.unshift(#{lib_dir.dump})
