4141e6a02256d17c47b966039e1e585ea4458e5b
Takashi Kokubun <takashikkbn@gmail.com>
2020-06-22 00:07:36 -0700

    Accept a relative path for specifying job type

--- lib/benchmark_driver/job_parser.rb
+++ lib/benchmark_driver/job_parser.rb
@@ -3,24 +3,27 @@ require 'benchmark_driver/runner'
 module BenchmarkDriver
   class << JobParser = Module.new
     # @param [Hash] config
-    # @param [Hash] default_params - Special default values not written in job definition
-    def parse(config, default_params: {})
+    # @param [Hash] working_directory - YAML-specific special parameter for "command_stdout" and a relative path in type
+    def parse(config, working_directory: nil)
       config = symbolize_keys(config)
       type = config.fetch(:type)
       if !type.is_a?(String)
         raise ArgumentError.new("Invalid type: #{config[:type].inspect} (expected String)")
-      elsif !type.match(/\A[A-Za-z0-9_]+\z/)
-        raise ArgumentError.new("Invalid type: #{config[:type].inspect} (expected to include only [A-Za-z0-9_])")
+      elsif !type.match(/\A[A-Za-z0-9_\/]+\z/)
+        raise ArgumentError.new("Invalid type: #{config[:type].inspect} (expected to include only [A-Za-z0-9_\/])")
       end
       config.delete(:type)
 
       # Dynamic dispatch for plugin support
-      require "benchmark_driver/runner/#{type}"
+      if type.include?('/')
+        require File.join(working_directory || '.', type)
+        type = File.basename(type)
+      else
+        require "benchmark_driver/runner/#{type}"
+      end
       job = ::BenchmarkDriver.const_get("Runner::#{camelize(type)}::JobParser", false).parse(**config)
-      default_params.each do |key, value|
-        if job.respond_to?(key) && job.respond_to?("#{key}=") && job.public_send(key).nil?
-          job.public_send("#{key}=", value)
-        end
+      if job.respond_to?(:working_directory) && job.respond_to?(:working_directory=) && job.working_directory.nil?
+        job.working_directory = working_directory
       end
       job
     end
